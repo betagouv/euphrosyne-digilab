4a49f5d5e4014d039c65cb52066f42f002a52527
witold <witoldwrob@gmail.com>
2023-10-30 15:38:48 +0100

    fix: integrate gatsby link to dsfr to prevent full reload

--- src/StartDsfr.tsx
+++ src/StartDsfr.tsx
@@ -2,8 +2,20 @@
 
 import { startReactDsfr } from "@codegouvfr/react-dsfr/next-appdir";
 import { defaultColorScheme } from "./defaultColorScheme";
+import { Link as GatsbyLink } from "gatsby";
+import { RegisteredLinkProps } from "@codegouvfr/react-dsfr/link";
 
-startReactDsfr({ defaultColorScheme });
+const Link = ({ ...props }: RegisteredLinkProps) => {
+  const { ref, href, ...newProps } = props;
+  return <GatsbyLink to={href || "#"} {...newProps} />;
+};
+declare module "@codegouvfr/react-dsfr/next-appdir" {
+  interface RegisterLink {
+    GatsbyLink: typeof Link;
+  }
+}
+
+startReactDsfr({ defaultColorScheme, Link });
 
 export function StartDsfr() {
   //Yes, leave null here.
