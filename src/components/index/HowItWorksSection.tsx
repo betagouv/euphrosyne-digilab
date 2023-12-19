391076962603f7997068c9d5ca92206a791364a8
witold <witoldwrob@gmail.com>
2023-12-19 11:54:44 +0100

    fix StaticImage rendering

--- src/components/index/HowItWorksSection.tsx
+++ src/components/index/HowItWorksSection.tsx
@@ -1,29 +1,49 @@
 import { css } from "@emotion/react";
 import { StaticImage } from "gatsby-plugin-image";
+import { IStaticImageProps } from "gatsby-plugin-image/dist/src/components/static-image.server";
 
 import { BaseSection } from "../BaseSection";
 
 interface ContentElement {
-  imageSrc: string;
-  imageAlt: string;
+  image: React.ReactElement<IStaticImageProps>;
   text: string;
 }
 
 export const HowItWorksSection = () => {
+  const defaultStaticImageProps: Omit<Omit<IStaticImageProps, "src">, "alt"> = {
+    placeholder: "blurred",
+    width: 80,
+    height: 80,
+  };
   const content: ContentElement[] = [
     {
-      imageSrc: "../../images/illustrations/search.svg",
-      imageAlt: "Icône de recherche",
+      image: (
+        <StaticImage
+          src="../../images/illustrations/search.svg"
+          alt="Icône de recherche"
+          {...defaultStaticImageProps}
+        />
+      ),
       text: "Un catalogue référençant les jeux de données produits par NewAglae depuis le 01/04/2022.",
     },
     {
-      imageSrc: "../../images/illustrations/calendar.svg",
-      imageAlt: "Icône de calendrier",
+      image: (
+        <StaticImage
+          src="../../images/illustrations/calendar.svg"
+          alt="Icône de calendrier"
+          {...defaultStaticImageProps}
+        />
+      ),
       text: "Une plateforme numérique permettant aux utilisateurs de NewAglae de préparer leurs expériences.",
     },
     {
-      imageSrc: "../../images/illustrations/human-cooperation.svg",
-      imageAlt: "Icône représentant la coopération humaine",
+      image: (
+        <StaticImage
+          src="../../images/illustrations/human-cooperation.svg"
+          alt="Icône représentant la coopération humaine"
+          {...defaultStaticImageProps}
+        />
+      ),
       text: "Un bureau virtuel pour que les utilisateurs de NewAglae puissent traiter et récupérer leurs données à distance.",
     },
   ];
@@ -40,13 +60,7 @@ export const HowItWorksSection = () => {
       <div className="fr-grid-row fr-grid-row--gutters">
         {content.map((element) => (
           <div className="fr-col-12 fr-col-lg-4 fr-p-7w">
-            <StaticImage
-              src={element.imageSrc}
-              alt={element.imageAlt}
-              placeholder="blurred"
-              width={80}
-              height={80}
-            />
+            {element.image}
             <p>{element.text}</p>
           </div>
         ))}
